2143eb2ff18b99ccb8c4f8b7f0d29ae4b2a584a8
AlesciDavide <davi.alesci@gmail.com>
2024-09-04 15:16:01 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\HomeController as GuestHomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [GuestHomeController::class, 'index'])->name('home');
+
+route::middleware('auth')->name('admin.')->prefix('admin/')->group(
+    function(){
+        /* rotte protette */
+    }
+);
+
