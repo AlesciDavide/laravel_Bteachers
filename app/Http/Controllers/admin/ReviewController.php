23176fd62ac7f6e9ceb05f24e6f1759b19f4e90b
MarcoMpensabene <marcompensabene@gmail.com>
2024-09-05 11:06:19 +0200

    added create and store

--- app/Http/Controllers/admin/ReviewController.php
+++ app/Http/Controllers/admin/ReviewController.php
@@ -22,7 +22,8 @@ class ReviewController extends Controller
      */
     public function create()
     {
-        //
+        $review = new Review();
+        return view('reviews.create', compact('review'));
     }
 
     /**
@@ -30,7 +31,10 @@ class ReviewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newReview = Review::create($data);
+
+        return redirect()->route('admin.reviews.show', $newReview);
     }
 
     /**
