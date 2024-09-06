f9667c18c968b7f363317baeedbd0479e1236b56
GencoMattia <gencomattia@hotmail.it>
2024-09-06 16:58:12 +0200

    mifrate profile_sponsor table

---
+++ database/migrations/2024_09_06_145517_create_profile_sponsor_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_sponsor', function (Blueprint $table) {
+            $table->unsignedBigInteger("profile_id");
+            $table->foreign("profile_id")->references("id")->on("profiles");
+
+            $table->unsignedBigInteger("sponsor_id");
+            $table->foreign("sponsor_id")->references("id")->on("sponsors");
+
+            $table->primary(["profile_id", "sponsor_id"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile_sponsor');
+    }
+};
