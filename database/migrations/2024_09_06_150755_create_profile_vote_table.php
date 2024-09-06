db8805f9f54c962d910535f4af60867013c840f8
GencoMattia <gencomattia@hotmail.it>
2024-09-06 17:08:56 +0200

    added profile_vote table migration

---
+++ database/migrations/2024_09_06_150755_create_profile_vote_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_vote', function (Blueprint $table) {
+            $table->unsignedBigInteger("profile_id");
+            $table->foreign("profile_id")->references("id")->on("profiles");
+
+            $table->unsignedBigInteger("vote_id");
+            $table->foreign("vote_id")->references("id")->on("votes");
+
+            $table->primary(["profile_id", "vote_id"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile_vote');
+    }
+};
